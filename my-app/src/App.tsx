0d3f1d6fd32b25f0f27a226268d156e5cb59c0cf
frederic <flausson@normandiewebschool.fr>
2025-03-05 11:00:33 +0100

    stopwatch status display

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,36 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Stopwatch } from './components/Stopwatch'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [isStopwatchPaused, setIsStopwatchPaused] = useState(false);
-  
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chronomètre</h1>
       <Stopwatch onPauseChange={setIsStopwatchPaused} />
+      <p>
+        État du chronomètre : {isStopwatchPaused ? 'En pause' : 'En marche'}
+      </p>
     </>
   )
 }
