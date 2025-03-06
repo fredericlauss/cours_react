b059b2e7f207bc17e12d23190e46c0e4dc3c2be4
frederic <flausson@normandiewebschool.fr>
2025-03-06 09:58:27 +0100

    Add React Router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,19 @@
+import { Link, Route, Routes } from 'react-router';
 import './App.css'
 import { Game } from './components/game';
+import { CGU } from './components/CGU';
 
 function App() {
 
   return (
     <div className="app">
-      <Game />
+      <nav>
+        <Link to="/">Accueil</Link> | <Link to="/cgu">CGU</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Game />} />
+        <Route path="/cgu" element={<CGU />} />
+      </Routes>
     </div>
   );
 }
---
+++ my-app/src/components/CGU.tsx
@@ -0,0 +1,17 @@
+export function CGU() {
+    return (
+      <div className="cgu">
+        <h1>Conditions Générales d'Utilisation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+          Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+          Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+        </p>
+        <p>
+          Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
+          Pellentesque congue. Ut in risus volutpat libero pharetra tempor. 
+          Cras vestibulum bibendum augue.
+        </p>
+      </div>
+    );
+  }
